56e0ea8eb7f3c74493cb6d21427c63fbe5e8ee35
Peiwen Lu <hi@peiwen.lu>
2025-01-04 16:29:51 +0800

    Fix the script path in vite plugin

--- plugins/vite.js
+++ plugins/vite.js
@@ -3,10 +3,7 @@ export default function pixelMirrorPlugin() {
     name: "vite-plugin-pixel-mirror",
     transformIndexHtml(html) {
       if (process.env.NODE_ENV !== "production") {
-        return html.replace(
-          "</body>",
-          `<script type="module" src="/node_modules/@p233-studio/pixel-mirror/dist/index.js"></script></body>`
-        );
+        return html.replace("</body>", `<script defer src="/node_modules/pixel-mirror/dist/index.js"></script></body>`);
       }
       return html;
     }
